eed635862e0e42335ea809847a7cd5f87f73aae9
RychidM <richardsk2@gmail.com>
2024-01-03 21:35:31 +0000

    Add new assets and update launcher icons

---
+++ lib/app/data/constants/app_colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class EzAppColors {
+  EzAppColors._();
+
+  static const ezPurple = Color(0xFF504DE4);
+  static const ezPurpleAlt = Color(0xFF6563E7);
+  static const ezGreenDarkMode = Color(0xFF00FF48);
+  static const ezGreen = Color(0xFF00E541);
+  static const ezPrimaryTextColor = Color(0xFF0D0D0D);
+  static const ezSecondaryTextColor = Color(0xFF8D8D8D);
+  static const ezWhite = Color(0xFFFFFFFF);
+  static const ezBlack = Color(0xFF000000);
+  static const ezProgressOrange = Color(0xFFFFA800);
+  static const ezCancelRed = Color(0xFFFF0000);
+  static const ezBorderGrey = Color(0xFFE6E6E6);
+  static const ezDarkModeBorderGrey = Color(0xFF262626);
+  static const ezBorderGreyAlt = Color(0xFFEFEFEF);
+  static const ezDarkModeBorderGreyAlt = Color(0xFF1C1C1C);
+  static const ezCardBgColor = Color(0xFFF8F8F8);
+  static const ezDarkModeCardBgColor = Color(0xFF131313);
+  static const ezBlue = Color(0xFF2A86FF);
+  static const ezTransparentBlue = Color(0x332A86FF);
+}
